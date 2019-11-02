62ba41c45de0d5490c97d8399f316a3b4ace034e
TestTaskDemo <igor-ner@mail.ru>
2019-11-02 13:24:13 +0200

    Add ViewModel

---
+++ ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp
+{
+	class ViewModel : INotifyPropertyChanged
+	{
+		private string selectedPath;
+		private ICommand openDialogCommand;
+
+		public ViewModel()
+		{
+			openDialogCommand = new RelayCommand(OpenDialog);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public string SelectedPath
+		{
+			get
+			{
+				return selectedPath;
+			}
+			set
+			{
+				if (selectedPath == value) return;
+				selectedPath = value;
+				OnPropertyChanged(nameof(selectedPath));
+			}
+		}
+
+		public ICommand OpenDialogCommand
+		{
+			get
+			{
+				return openDialogCommand;
+			}
+		}
+
+		public void OnPropertyChanged([CallerMemberName]string prop = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+
+		public void OpenDialog()
+		{
+
+		}
+	}
+}
