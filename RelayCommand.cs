0cde96ca9bd1b7e7673f6cb66452d9fa62b72078
TestTaskDemo <igor-ner@mail.ru>
2019-11-02 13:17:39 +0200

    Add RelayCommand

---
+++ RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+	class RelayCommand : ICommand
+	{
+		private Action _action;
+
+		public RelayCommand(Action passedAction)
+		{
+			_action = passedAction;
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			_action();
+		}
+	}
+}
